92fdfbeac32a0d2a94214c3efa6ea029896f3b59
Andrew Lee <alee14498@protonmail.com>
2020-05-10 18:57:34 -0400

    Added NuxtJS Auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ['@nuxtjs/pwa'],
+  modules: ['@nuxtjs/pwa', '@nuxtjs/axios', '@nuxtjs/auth'],
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
